f9fd477ea04628da62a2c47ab7816c57dfeadd9b
sdi2000266 <sdi2000266@di.uoa.gr>
2024-04-17 13:58:00 +0300

    bg image

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { AspectRatio, Box, Flex, Image } from '@chakra-ui/react';
+import { AspectRatio, Box, Flex, Image, Text } from '@chakra-ui/react';
 import { Card, CardBody } from '@chakra-ui/react';
 import bg from './images/bg.png';
 
 const MainPage = () => {
     return (
-        <Flex flexDirection='column' alignItems='center' bg='#4692f0'>
-            <Flex justifyContent='center' paddingTop="20px">
+        <Flex flexDirection='column' alignItems='center' bgImage={`url(${bg})`} >
+            <Flex justifyContent='center' paddingTop="20px" paddingBottom="300px">
                 <Card bg='white' boxShadow='lg' borderRadius='lg' width='900px'>
-                    <CardBody>
+                    <CardBody p="30px">
                         <AspectRatio ratio={16/9}>
                             <iframe
                                 title='abracadabra'
@@ -16,10 +16,14 @@ const MainPage = () => {
                                 allowFullScreen
                             />
                         </AspectRatio>
+                        <Text mt="4" textAlign="center">Το <strong>ΑΜΠΡΑ ΚΑΤΑΜΠΡΑ</strong>, ένα πλούσιο έργο με την έγκριση 
+                        του Υπ.Παιδείας και Θρησκευμάτων, περιλαμβάνει τον κόσμο της παιδικής λογοτεχνίας σε 26
+                         πολύχρωμα βιβλία με την υπέροχη εικονογράφηση από ειδικευμένους ζωγράφους καθώς και 26 cd 
+                         με ζωντανή θεατρική απόδοση από τους καλύτερους έλληνες ηθοποιούς...
+                         </Text>
                     </CardBody>
                 </Card>
             </Flex>
-            <Image src={bg} maxWidth='100%' paddingTop='100px'/>
         </Flex>
     );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,27 +17,53 @@ const NavBar = () => {
       <Center>
         <Image src={logo} maxW="200px" />
       </Center>
-      <Center style={{ backgroundColor: "white"}} >
-      <Flex  style={{ backgroundColor: "white"}}>
-        <Button 
-         size="lg" variant="outline" color="#c7260c" borderColor="#c7260c" width="200px"
-         _hover={{ bg: "#ad3b00" }} borderRadius="0px" height="100px"  onClick={navigateToHome} border="0px"
-         >
-          ΑΡΧΙΚΗ
-        </Button>
-        <Button size="lg" variant="outline" color="#c7260c" borderColor="#c7260c" 
-                width="200px" _hover={{ bg: "#ad3b00" }} borderRadius="0px" height="100px" border="0px">
-          ΒΙΒΛΙΑ
-        </Button>
-        <Button
-          size="lg"
-          leftIcon={<MdCall />}
-          variant="outline" color="#c7260c" borderColor="#c7260c" _hover={{ bg: "#ad3b00" }} borderRadius="0px" height="100px" border="0px"
-          onClick={navigateToContact}
-        >
-          ΕΠΙΚΟΙΝΩΝΙΑ
-        </Button>
-      </Flex>
+      <Center style={{ backgroundColor: "white" }}>
+        <Flex style={{ backgroundColor: "white" }}>
+          <Button
+            size="lg"
+            variant="outline"
+            color="#c7260c"
+            borderColor="#c7260c"
+            width="200px"
+            _hover={{ bg: "#ad3b00", color: "white" }} // Add color: "white" here
+            borderRadius="0px"
+            height="100px"
+            onClick={navigateToHome}
+            border="0px"
+          >
+            ΑΡΧΙΚΗ
+          </Button>
+          <Button
+            size="lg"
+            variant="outline"
+            color="#c7260c"
+            borderColor="#c7260c"
+            width="200px"
+            _hover={{ bg: "#ad3b00", color: "white" }} // Add color: "white" here
+            borderRadius="0px"
+            height="100px"
+            border="0px"
+          >
+            ΒΙΒΛΙΑ
+          </Button>
+          <Button
+            size="lg"
+            leftIcon={<MdCall />}
+            variant="outline"
+            color="#c7260c"
+            borderColor="#c7260c"
+            _hover={{ bg: "#ad3b00", color: "white" }} // Add color: "white" here
+            borderRadius="0px"
+            height="100px"
+            border="0px"
+            onClick={navigateToContact}
+          >
+            ΕΠΙΚΟΙΝΩΝΙΑ
+          </Button>
+        </Flex>
+      </Center>
+      <Center>
+        <div style={{ backgroundColor: "#c7260c", height: "2px", width: "100%" }}></div>
       </Center>
     </div>
   );
